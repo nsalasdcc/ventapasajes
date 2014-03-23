9352dd37e144b1d24e5b85ca9cbbb735fdc69f85
Nicolas Salas <nikosalas@gmail.com>
2014-03-23 03:58:03 -0300

    Creado template para vistas. Arregladas urls y vistas.

--- venta/urls.py
+++ venta/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from venta import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^recorrido/(?P<id_recorrido>\d+)/$', views.detalle, name='detalle'),
 )
--- venta/views.py
+++ venta/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader, RequestContext
+from venta.models import Recorrido
 
 
 def index(request):
-    return HttpResponse('Hello. World!')
+    lista_recorridos = Recorrido.objects.all()
+
+    context = {
+        'lista_recorridos': lista_recorridos
+    }
+
+    return render(request, "venta/index.html", context)
+
+
+def detalle(request, id_recorrido):
+    recorrido = get_object_or_404(Recorrido, id_recorrido=id_recorrido)
+
+    context = {
+        'recorrido': recorrido
+    }
+
+    return render(request, "venta/detalle.html", context)
