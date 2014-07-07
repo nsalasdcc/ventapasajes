f5ead0d945e89c350163fd3161d20d73c4dbca21
Nicolas Salas <nikosalas@gmail.com>
2014-07-07 00:19:01 -0400

    Añadido backup de la base de datos

--- ventapasajes/settings.py
+++ ventapasajes/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'django.contrib.admin',
+    'django.contrib.sessions',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.messages',
